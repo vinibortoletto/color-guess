96a0b4c754face69df9691bee409ba8a401433c1
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-07-09 19:22:25 -0300

    remoção de importação desnecessária

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import generateRandomColor from './scripts/generateRandomColor.js';
 import createColorOptions from './scripts/createColorOptions.js';
 import getRandomColorFromOptions from './scripts/getRandomColorFromOptions.js';
 import resetGame from './scripts/resetGame.js';
